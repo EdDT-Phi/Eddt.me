6c85d7ad1f680af19312148abe89f97ab6264dee
EdDT-Phi <EdDT.Phi@gmail.com>
2016-08-12 13:12:29 -0400

    standard headers and footers

---
+++ public/about/js/controllers/generic.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+var app = angular.module('generic', []);
+
+app.controller('genericCtrl', [
+'$scope', '$http',
+function($scope, $http){
+
+
+  var init = function() {
+
+  }
+
+  init();
+}]);
+
+
